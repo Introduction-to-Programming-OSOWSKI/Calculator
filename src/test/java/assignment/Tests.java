2a1bfe6afff611c324112d39b7d01b22bfe4f4c7
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-24 20:52:45 +0000

    added due date logic

--- src/test/java/assignment/Tests.java
+++ src/test/java/assignment/Tests.java
@@ -14,12 +14,19 @@ public class Tests {
 
 
    @Test
-   public void test(){
+   public void code(){
        App.main(null);
        assertEquals(115, App.calculate("add", 15, 100), "ADDITION FAILED");
        assertEquals(40, App.calculate("subtract", 50, 10), "SUBTRACTION FAILED");
        assertEquals(36, App.calculate("multiply", 3, 12), "MULTIPLICATION FAILED");
        assertEquals(2.5f, App.calculate("divide", 10, 4), "DIVISION FAILED");
    }
+
+   @Test
+    public void late() {
+        App.main(null);
+        assertTrue(DueDate.onTime(2020, 11, 24), "Submitted Late");
+    }
+
 
 }
